d7ef0eb3f19f365e9357e4d5511df7ab05b42fc0
afshin <afshindr67@yahoo.com>
2022-07-01 14:56:28 +0430

    Add existing project files to Git

---
+++ src/main/java/com/example/dokan11/Dokan11Application.java
@@ -0,0 +1,13 @@
+package com.example.dokan11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dokan11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dokan11Application.class, args);
+    }
+
+}
